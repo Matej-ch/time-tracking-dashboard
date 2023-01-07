32a9fb1154b834badd53185d79e3db2694e22fd1
Matej <matejchalachan@gmail.com>
2023-01-07 18:11:29 +0100

    added user preview

--- src/DashboardUserProfile.tsx
+++ src/DashboardUserProfile.tsx
@@ -1,6 +1,20 @@
-function DashboardUserProfile() {
-    return (<article>
+import './DashboardUserProfile.scss'
+import img from './assets/image-jeremy.png'
 
+function DashboardUserProfile() {
+    return (<article className={'user-profile'}>
+        <div className={'front'}>
+            <img src={img} alt="Image of Jeremy robson" width={'64'}/>
+            <div className={'name'}>
+                <span>Report for</span>
+                <span>Jeremy Robson</span>
+            </div>
+        </div>
+        <div className={'back'}>
+            <a href="">Daily</a>
+            <a href="">Weekly</a>
+            <a href="">Monthly</a>
+        </div>
     </article>);
 }
 
